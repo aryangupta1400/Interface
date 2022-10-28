38ea6c785e396fcfc16084edf390f698200e81d1
Aryan.Gupta <Aryan.Gupta@BRD-3917L13-L>
2022-10-28 22:23:32 +0530

    Add project files.

---
+++ Interface/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// 4) Create Interface for Fruits and implement method in diffrent fruits.
+//    Then print name and color of that fruits.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
