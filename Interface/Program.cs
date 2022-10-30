a1b49b7f02a7418065282198510a6a36db905512
Aryan Gupta <Aryan.Gupta@oneadvanced.com>
2022-10-30 18:43:25 +0530

    Coding Standards updated ad code modified

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -14,6 +14,48 @@ namespace Interface
     {
         static void Main(string[] args)
         {
+            Apple apple = new Apple();
+            apple.FruitColor();
+            apple.VitaminContent();
+
+            Mango mango = new Mango();
+            mango.FruitColor();
+            mango.VitaminContent();
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IFruit
+    {
+        void FruitColor();
+        
+        void VitaminContent();
+    }
+
+    class Apple : IFruit
+    {
+        public void FruitColor()
+        {
+            Console.WriteLine("\nApples are RED in color.");
+        }
+
+        public void VitaminContent()
+        {
+            Console.WriteLine("Apples have high Vitamin C and Vitamin E content.\n");
+        }
+    }
+
+    class Mango : IFruit
+    {
+        public void FruitColor()
+        {
+            Console.WriteLine("\nMango are YELLOW in color.");
+        }
+
+        public void VitaminContent()
+        {
+            Console.WriteLine("Apples have high Vitamin A, Vitamin C and Vitamin K content.\n");
         }
     }
 }
